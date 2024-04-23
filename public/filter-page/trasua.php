3c1a5961c3c409e2b99c3df9508549516c9df547
Nguyễn Huy <huyxcbn@gmail.com>
2024-04-23 16:48:32 +0700

    first commit

---
+++ public/filter-page/trasua.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sản Phẩm Trà Sữa</title>
+</head>
+<body>
+    <div style="margin-left: 5px;">
+        <div class="container-content-list-product">
+            <div class="list-product-all-item">
+                <?php
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "z9milktea";
+
+                $conn = new mysqli($servername, $username, $password, $dbname);
+
+                if ($conn->connect_error) {
+                    die("Kết nối không thành công: " . $conn->connect_error);
+                }
+
+                $sql = "SELECT products.*, product_sizes.product_price
+                        FROM products
+                        INNER JOIN product_sizes ON products.product_id = product_sizes.product_id
+                        WHERE products.category_id IN (SELECT category_id FROM categorys WHERE category_name = 'Trà Sữa')";
+
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo '<div class="product-item">';
+                        echo '<a href="show-detail.php?product_id=' . $row['product_id'] . '" class="container-img">';
+                        echo "<img class='product-img' src='admin/productAD/uploads/" . htmlspecialchars($row['product_img']) . "' alt='Image of " . htmlspecialchars($row["product_name"]) . "'>";
+                        echo '</a>';
+                        echo '<p class="product-name">' . $row['product_name'] . '</p>';
+                        echo '<div class="product-des">' . $row['product_des'] . '</div>';
+
+                        echo '<div class="product-price">' . $row['product_price'] . '</div>';
+                        echo '<div class="action-add">';
+                        echo '<button type="submit" class="add-to-cart">';
+                        echo '<i class="fa-solid fa-cart-shopping"></i>';
+                        echo '</button>';
+                        echo '</div>';
+                        echo '</div>';
+                    }
+                } else {
+                    echo "Không có sản phẩm Trà Sữa.";
+                }
+
+                $conn->close();
+                ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ public/footer.php
@@ -0,0 +1,42 @@
+<section class="footer-left">
+    <div class="footer-address">
+        <p class="footer-text">
+            <b>Address:</b>
+           Số 133, ngõ 296, đường lĩnh lam, phường Lĩnh Nam, Hoàng Mai, Hà Nội
+        </p>
+
+        <p class="footer-text">
+            <b>Tel:</b>
+            09128461942
+        </p>
+
+        <p class="footer-text">
+            <b>Email:</b>
+            Trasuahoney@gmail.com
+        </p>
+    </div>
+
+    <div class="footer-contact">
+        <!-- <img src="./images/fb-logo.jpg" alt="FB">
+        <img src="./images/logo-ins.jpg" alt="INS">
+        <img src="./images/logo-gg.png" alt="GG"> -->
+    </div>
+</section>
+
+<style>
+    .footer-left {
+        width: 30%;
+        margin: 0 auto; /* Căn giữa theo chiều ngang */
+        text-align: center; /* Căn giữa nội dung theo chiều ngang */
+    }
+
+    .footer-address, .footer-contact {
+        margin-top: 20px;
+    }
+
+    .footer-text {
+        margin-bottom: 10px;
+    }
+
+    /* Các quy tắc CSS khác ở đây */
+</style>
